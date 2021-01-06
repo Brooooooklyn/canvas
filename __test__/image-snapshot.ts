e4cde24fd26d31ef9909799e7d196f472a86e711
LongYinan <lynweklm@gmail.com>
2021-01-06 16:04:41 +0800

    ci: upload failure images

--- __test__/image-snapshot.ts
+++ __test__/image-snapshot.ts
@@ -1,13 +1,21 @@
 import { promises as fs } from 'fs'
 import { join } from 'path'
 
+import PNG from '@jimp/png'
 import { ExecutionContext } from 'ava'
 
-export async function snapshotImage<C>(t: ExecutionContext<C>, ext = 'png') {
+const png = PNG()
+
+export async function snapshotImage<C>(t: ExecutionContext<C>) {
   // @ts-expect-error
   const { canvas } = t.context
   const image = await canvas.png()
-  const p = join(__dirname, 'snapshots', `${t.title}.${ext}`)
+  const p = join(__dirname, 'snapshots', `${t.title}.png`)
+
+  async function writeFailureImage() {
+    await fs.writeFile(join(__dirname, 'failure', `${t.title}.png`), image)
+  }
+
   let existed = true
   try {
     await fs.stat(p)
@@ -19,6 +27,24 @@ export async function snapshotImage<C>(t: ExecutionContext<C>, ext = 'png') {
     t.pass()
   } else {
     const existed = await fs.readFile(p)
-    t.deepEqual(image, existed)
+    t.notThrowsAsync(async () => {
+      const existedPixels = png.decoders['image/png'](existed).data
+      const imagePixels = png.decoders['image/png'](image).data
+      if (existedPixels.length !== imagePixels.length) {
+        await writeFailureImage()
+        throw new Error('Image size is not equal')
+      }
+      let diffCount = 0
+      imagePixels.forEach((u8, index) => {
+        if (u8 !== existedPixels[index]) {
+          diffCount++
+        }
+      })
+      // diff ratio greater than 0.01%
+      if (diffCount / existedPixels.length > 0.01 / 100) {
+        await writeFailureImage()
+        throw new Error('Image bytes is not equal')
+      }
+    })
   }
 }
