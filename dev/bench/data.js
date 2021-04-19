2205d0215976407cc875e99e721a75db0ca5cbab
github-action-benchmark <github@users.noreply.github.com>
2021-04-19 06:39:59 +0000

    add Benchmark (benchmarkjs) benchmark result for a61d3707a344f90f03476fb372d2e086c8bdb4be

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618672870744,
+  "lastUpdate": 1618814399215,
   "repoUrl": "https://github.com/Brooooooklyn/canvas",
   "entries": {
     "Benchmark": [
@@ -2960,6 +2960,58 @@ window.BENCHMARK_DATA = {
             "range": "±0.11%",
             "unit": "ops/sec",
             "extra": "46 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lynweklm@gmail.com",
+            "name": "LongYinan",
+            "username": "Brooooooklyn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a61d3707a344f90f03476fb372d2e086c8bdb4be",
+          "message": "ci: use workflow_run event rather than check_suite",
+          "timestamp": "2021-04-19T14:34:28+08:00",
+          "tree_id": "211e6167f7825c89bd8cf138b9b133853d219692",
+          "url": "https://github.com/Brooooooklyn/canvas/commit/a61d3707a344f90f03476fb372d2e086c8bdb4be"
+        },
+        "date": 1618814320888,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Draw house#@napi-rs/skia",
+            "value": 20,
+            "range": "±1.3%",
+            "unit": "ops/sec",
+            "extra": "37 samples"
+          },
+          {
+            "name": "Draw house#node-canvas",
+            "value": 21,
+            "range": "±1.2%",
+            "unit": "ops/sec",
+            "extra": "39 samples"
+          },
+          {
+            "name": "Draw gradient#@napi-rs/skia",
+            "value": 19,
+            "range": "±1.96%",
+            "unit": "ops/sec",
+            "extra": "40 samples"
+          },
+          {
+            "name": "Draw gradient#node-canvas",
+            "value": 21,
+            "range": "±1.21%",
+            "unit": "ops/sec",
+            "extra": "38 samples"
           }
         ]
       }
