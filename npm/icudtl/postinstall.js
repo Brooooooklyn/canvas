efd0c48521b83da9c643cff048fc425819cbf4a2
LongYinan <lynweklm@gmail.com>
2021-05-06 11:12:10 +0800

    chore: simplify postinstall scripts in icudtl

--- npm/icudtl/postinstall.js
+++ npm/icudtl/postinstall.js
@@ -3,8 +3,7 @@ const { join } = require('path')
 
 const ICU_DAT = 'icudtl.dat'
 
-const path = join(require.resolve('@napi-rs/canvas', '..'))
-copyFile(join(__dirname, ICU_DAT), join(path, '..', ICU_DAT), (err) => {
+copyFile(join(__dirname, ICU_DAT), join(require.resolve('@napi-rs/canvas'), '..', ICU_DAT), (err) => {
   if (err) {
     console.error(err)
     process.exit(1)
