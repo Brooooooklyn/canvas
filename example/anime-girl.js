e6e88589b7f0907b59a387e99775ca1334f0aa94
yisibl <percyley@qq.com>
2021-10-25 13:23:07 +0800

    docs: add complex svg example

---
+++ example/anime-girl.js
@@ -0,0 +1,37 @@
+const fs = require('fs').promises
+const { join } = require('path')
+const { performance } = require('perf_hooks')
+
+const { createCanvas, Image } = require('../index')
+/* eslint-disable no-console */
+
+async function main() {
+  const file = await fs.readFile(join(__dirname, './anime-girl.svg'))
+
+  const t0 = performance.now()
+  const image = new Image()
+  image.src = file
+
+  const w = 1052
+  const h = 744
+
+  // resize SVG
+  image.width = w
+  image.height = h
+
+  // create a canvas of the same size as the image
+  const canvas = createCanvas(w, h)
+  const ctx = canvas.getContext('2d')
+
+  // fill the canvas with the image
+  ctx.drawImage(image, 0, 0)
+
+  const pngData = await canvas.encode('png')
+  await fs.writeFile(join(__dirname, 'anime-girl.png'), pngData)
+
+  const t1 = performance.now()
+
+  console.log('✨ Done in', t1 - t0, 'ms')
+}
+
+main()
