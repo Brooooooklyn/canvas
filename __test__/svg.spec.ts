0f5a1be9c82ba2c67e2dcf67a327c1847f79bbb1
LongYinan <lynweklm@gmail.com>
2022-05-29 11:53:05 +0800

    win32 svg

--- __test__/svg.spec.ts
+++ __test__/svg.spec.ts
@@ -1,6 +1,5 @@
 import { join } from 'path'
 import { readFileSync, promises as fs } from 'fs'
-import { platform } from 'os'
 
 import test from 'ava'
 
@@ -14,9 +13,5 @@ test('convertSVGTextToPath should work', async (t) => {
   const result = convertSVGTextToPath(FIXTURE)
   const outputPath = join(__dirname, 'text-to-path.svg')
   const output = await fs.readFile(outputPath, 'utf8')
-  if (platform() === 'win32') {
-    t.true(result.toString('utf8').length > 13000)
-  } else {
-    t.deepEqual(result.toString('utf8'), output)
-  }
+  t.deepEqual(result.toString('utf8'), output)
 })
