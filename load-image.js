205d73cfc5b87b9b2513fb3aa985a0c6a5f0b863
rambo <luwei.li@17zuoye.com>
2022-08-17 16:14:24 +0800

    feat: add Buffer JSON.stringify obj

--- load-image.js
+++ load-image.js
@@ -86,6 +86,7 @@ function createImage(src, alt) {
 
 function isBufferLike(src) {
   return (
+    src?.type === "Buffer" ||
     Array.isArray(src) ||
     src instanceof ArrayBuffer ||
     src instanceof SharedArrayBuffer ||
