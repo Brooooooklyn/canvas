a904f81e3a8c2229b3566eb5386a44965e746f72
github-action-benchmark <github@users.noreply.github.com>
2022-08-26 02:42:17 +0000

    add Benchmark (benchmarkjs) benchmark result for f9b9fc4bd0a694987b370d409a323449339b05c3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1661416534362,
+  "lastUpdate": 1661481737837,
   "repoUrl": "https://github.com/Brooooooklyn/canvas",
   "entries": {
     "Benchmark": [
@@ -17588,6 +17588,72 @@ window.BENCHMARK_DATA = {
             "range": "±1.42%",
             "unit": "ops/sec",
             "extra": "46 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lynweklm@gmail.com",
+            "name": "LongYinan",
+            "username": "Brooooooklyn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f9b9fc4bd0a694987b370d409a323449339b05c3",
+          "message": "Merge pull request #546 from Brooooooklyn/upgrade-ava\n\nchore: upgrade ava to compatible with Node.js 18.8.0",
+          "timestamp": "2022-08-26T10:34:40+08:00",
+          "tree_id": "7782cc1221955ec14172d2e93182cd2be4deaad8",
+          "url": "https://github.com/Brooooooklyn/canvas/commit/f9b9fc4bd0a694987b370d409a323449339b05c3"
+        },
+        "date": 1661481664722,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Draw house#skia-canvas",
+            "value": 24,
+            "range": "±0.24%",
+            "unit": "ops/sec",
+            "extra": "43 samples"
+          },
+          {
+            "name": "Draw house#node-canvas",
+            "value": 26,
+            "range": "±0.58%",
+            "unit": "ops/sec",
+            "extra": "47 samples"
+          },
+          {
+            "name": "Draw house#@napi-rs/skia",
+            "value": 23,
+            "range": "±0.19%",
+            "unit": "ops/sec",
+            "extra": "42 samples"
+          },
+          {
+            "name": "Draw gradient#skia-canvas",
+            "value": 23,
+            "range": "±0.49%",
+            "unit": "ops/sec",
+            "extra": "42 samples"
+          },
+          {
+            "name": "Draw gradient#node-canvas",
+            "value": 25,
+            "range": "±0.38%",
+            "unit": "ops/sec",
+            "extra": "45 samples"
+          },
+          {
+            "name": "Draw gradient#@napi-rs/skia",
+            "value": 22,
+            "range": "±0.08%",
+            "unit": "ops/sec",
+            "extra": "41 samples"
           }
         ]
       }
