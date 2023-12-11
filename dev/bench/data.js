4d667ad00a5358ce3fd6ddc30df1d067631e784c
github-action-benchmark <github@users.noreply.github.com>
2023-12-11 03:53:51 +0000

    add Benchmark (benchmarkjs) benchmark result for e566efe07c86d5a4437c80b9da00f6255b1455e0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1699962548890,
+  "lastUpdate": 1702266831901,
   "repoUrl": "https://github.com/Brooooooklyn/canvas",
   "entries": {
     "Benchmark": [
@@ -25902,6 +25902,72 @@ window.BENCHMARK_DATA = {
             "name": "Draw gradient#@napi-rs/skia",
             "value": 28,
             "range": "±0.57%",
+            "unit": "ops/sec",
+            "extra": "50 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e566efe07c86d5a4437c80b9da00f6255b1455e0",
+          "message": "chore(deps): update actions/setup-python action to v5 (#753)\n\nCo-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>",
+          "timestamp": "2023-12-11T11:46:51+08:00",
+          "tree_id": "f002f8cd60eade767d458f6e32f976739051507e",
+          "url": "https://github.com/Brooooooklyn/canvas/commit/e566efe07c86d5a4437c80b9da00f6255b1455e0"
+        },
+        "date": 1702266784745,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Draw house#skia-canvas",
+            "value": 30,
+            "range": "±0.36%",
+            "unit": "ops/sec",
+            "extra": "53 samples"
+          },
+          {
+            "name": "Draw house#node-canvas",
+            "value": 37,
+            "range": "±0.65%",
+            "unit": "ops/sec",
+            "extra": "63 samples"
+          },
+          {
+            "name": "Draw house#@napi-rs/skia",
+            "value": 29,
+            "range": "±0.32%",
+            "unit": "ops/sec",
+            "extra": "51 samples"
+          },
+          {
+            "name": "Draw gradient#skia-canvas",
+            "value": 28.4,
+            "range": "±0.31%",
+            "unit": "ops/sec",
+            "extra": "50 samples"
+          },
+          {
+            "name": "Draw gradient#node-canvas",
+            "value": 36.1,
+            "range": "±0.47%",
+            "unit": "ops/sec",
+            "extra": "62 samples"
+          },
+          {
+            "name": "Draw gradient#@napi-rs/skia",
+            "value": 28.1,
+            "range": "±0.36%",
             "unit": "ops/sec",
             "extra": "50 samples"
           }
